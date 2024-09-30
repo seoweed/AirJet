33caee0d7da4deb6deee7dda13768c0ffc0dbd0c
ãseoweed <darkweed1106@gmail.com>
2024-10-01 00:09:20 +0900

    project setting

---
+++ src/main/java/com/meta/air_jet/AirJetApplication.java
@@ -0,0 +1,13 @@
+package com.meta.air_jet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirJetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirJetApplication.class, args);
+    }
+
+}
